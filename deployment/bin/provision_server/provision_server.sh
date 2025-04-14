bf41305edf90dd71c0aae5b85b84ca33a5c1f513
romanmahiiovych <romanmah1998@gmail.com>
2025-04-14 23:29:59 +0300

    fix deploy

--- deployment/bin/provision_server/provision_server.sh
+++ deployment/bin/provision_server/provision_server.sh
@@ -7,85 +7,83 @@ SSH_KEY=$2
 
 PROJECT_DIR="/var/www/html/posts"
 
+# -----------------------------
+# СТВОРЕННЯ ПАПКИ ПРОЄКТУ
+# -----------------------------
 mkdir -p $PROJECT_DIR
-
 chown -R www-data:www-data $PROJECT_DIR
-
 cd $PROJECT_DIR
 
 git config --global --add safe.directory $PROJECT_DIR
 
+# -----------------------------
+# КЛОНУВАННЯ ПРОЄКТУ
+# -----------------------------
 if [ ! -d $PROJECT_DIR"/.git" ]; then
-  GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes' git clone git@github.com:mmartinjoo/devops-with-laravel-sample.git .
-  cp $PROJECT_DIR"/api/.env.example" $PROJECT_DIR"/api/.env"
-  sed -i "/DB_PASSWORD/c\DB_PASSWORD=$MYSQL_PASSWORD" $PROJECT_DIR"/api/.env"
-  sed -i '/QUEUE_CONNECTION/c\QUEUE_CONNECTION=database' $PROJECT_DIR"/api/.env"
+  GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes' git clone git@github.com:RomanMahiiovych/fundamental.git .
+  cp ./api/.env.example ./api/.env
+  sed -i "/DB_PASSWORD/c\DB_PASSWORD=$MYSQL_PASSWORD" ./api/.env
+  sed -i '/QUEUE_CONNECTION/c\QUEUE_CONNECTION=database' ./api/.env
 fi
 
-# node & npm
-rm -f /usr/bin/node
-rm -f /usr/bin/npm
-rm -f /usr/bin/npx
+# -----------------------------
+# ВСТАНОВЛЕННЯ NODE ЧЕРЕЗ NVM
+# -----------------------------
+export NVM_DIR="$HOME/.nvm"
+if [ ! -d "$NVM_DIR" ]; then
+  curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+fi
+source "$NVM_DIR/nvm.sh"
 
-cd /usr/lib
-wget https://nodejs.org/dist/v14.21.3/node-v14.21.3-linux-x64.tar.xz
-tar xf node-v14.21.3-linux-x64.tar.xz
-rm node-v14.21.3-linux-x64.tar.xz
-mv ./node-v14.21.3-linux-x64/bin/node /usr/bin/node
-ln -s /usr/lib/node-v14.21.3-linux-x64/lib/node_modules/npm/bin/npm-cli.js /usr/bin/npm
-ln -s /usr/lib/node-v14.21.3-linux-x64/lib/node_modules/npx/bin/npx-cli.js /usr/bin/npx
+nvm install 14
+nvm use 14
+nvm alias default 14
 
-# php 8.1
+
+# -----------------------------
+# ВСТАНОВЛЕННЯ PHP 8.1 + РОЗШИРЕННЯ
+# -----------------------------
 add-apt-repository ppa:ondrej/php -y
 apt update -y
-apt install php8.1-common php8.1-cli -y
-apt install php8.1-dom -y
-apt install php8.1-gd -y
-apt install php8.1-zip -y
-apt install php8.1-curl -y
-apt install php8.1-mysql -y
-apt install php8.1-sqlite3 -y
-apt install php8.1-mbstring -y
-apt install php8.1-fpm -y
 
-apt install net-tools -y
-apt install supervisor -y
-apt install unzip
+apt install -y php8.1-common php8.1-cli php8.1-fpm \
+php8.1-dom php8.1-gd php8.1-zip php8.1-curl \
+php8.1-mysql php8.1-sqlite3 php8.1-mbstring \
+net-tools supervisor unzip curl git software-properties-common
 
+# -----------------------------
+# ВСТАНОВЛЕННЯ COMPOSER
+# -----------------------------
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') === true) { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/bin/composer
+mv composer.phar /usr/local/bin/composer
 
-mysql -uroot -p$MYSQL_PASSWORD < $PROJECT_DIR"/deployment/config/mysql/create_database.sql" || echo "Database already exists"
-mysql -uroot -p$MYSQL_PASSWORD < $PROJECT_DIR"/deployment/config/mysql/set_native_password.sql"
+composer -V
 
-echo "* * * * * cd $PROJECT_DIR && php artisan schedule:run >> /dev/null 2>&1" >> cron_tmp
-crontab cron_tmp
-rm cron_tmp
+# -----------------------------
+# СТВОРЕННЯ БАЗИ ДАНИХ
+# -----------------------------
+mysql -uroot -p$MYSQL_PASSWORD < ./deployment/config/mysql/create_database.sql || echo "Database already exists"
+mysql -uroot -p$MYSQL_PASSWORD < ./deployment/config/mysql/set_native_password.sql
 
-cp $PROJECT_DIR"/deployment/config/supervisor/logrotate" /etc/logrotate.d/supervisor
+# -----------------------------
+# СТВОРЕННЯ КОРИСТУВАЧА `roman`
+# -----------------------------
+useradd -G www-data,root -u 1000 -d /home/roman roman || true
+mkdir -p /home/roman/.ssh
+touch /home/roman/.ssh/authorized_keys
+echo "$SSH_KEY" >> /home/roman/.ssh/authorized_keys
+chown -R roman:roman /home/roman
+chmod 700 /home/roman/.ssh
+chmod 600 /home/roman/.ssh/authorized_keys
+echo "roman ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/roman
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-
-aws configure
-
-useradd -G www-data,root -u 1000 -d /home/martin martin
-mkdir -p /home/martin/.ssh
-touch /home/martin/.ssh/authorized_keys
-chown -R martin:martin /home/martin
-chown -R martin:martin /var/www/html
-chmod 700 /home/martin/.ssh
-chmod 644 /home/martin/.ssh/authorized_keys
-
-echo "$SSH_KEY" >> /home/martin/.ssh/authorized_keys
-
-echo "martin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/martin
-
+# -----------------------------
+# ФІНАЛЬНА ПЕРЕВІРКА
+# -----------------------------
 php -v
 node -v
 npm -v
-aws --version
+composer -V
