00029b6c7863752981f0f645d721219278dd2023
romanmahiiovych <romanmah1998@gmail.com>
2025-05-09 18:10:39 +0300

    add register and logout methods

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -5,10 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController
 {
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken,
+        ]);
+    }
+
     public function login(Request $request)
     {
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
@@ -22,5 +37,14 @@ class AuthController
         }
 
         return response('', Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out',
+        ]);
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('posts/export', [PostController::class, 'export'])->name('posts.export');
